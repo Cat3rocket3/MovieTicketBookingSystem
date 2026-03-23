1236ee72e29d647d32711b391779ac9acaa4c232
Cat3rocket <ca3rocket3@gmail.com>
2026-03-23 21:22:34 +0200

    Add project files.

---
+++ MovieTickets/Domain/Entities/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTickets.Domain.Entities
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        SoapDuration Duration { get; set; }
+
+       public decimal Price { get; set; }    
+        
+        public Movie(int id, string name, SoapDuration duration, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Duration = duration;
+            Price = price;
+        }
+
+    }
+}
---
+++ MovieTickets/Domain/Entities/Projection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTickets.Domain.Entities
+{
+    internal class Projection
+    {
+        Movie Movie { get; set; }
+        Hall Hall { get; set; }
+        decimal Price { get; set; }
+        DateTime Date { get; set; }
+
+
+        public Projection(Movie movie, Hall hall, decimal price, DateTime date)
+        {
+            Movie = movie;
+            Hall = hall;
+            Price = Movie.Price;
+            Date = date;
+        }
+
+    }
+}
