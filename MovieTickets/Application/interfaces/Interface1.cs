8314547bf995b5deda490019c28931b9886b3b99
Cat3rocket <ca3rocket3@gmail.com>
2026-03-24 12:11:58 +0200

    added an interface

---
+++ MovieTickets/Application/interfaces/Interface1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTickets.Application.interfaces
+{
+    public interface ITheaterRepository
+    {
+        IReadOnlyList<Movie> GetAllMovies();
+        Movie GetMovieById(int id);
+        void AddMovie(Movie movie);
+
+        IReadOnlyList<Hall> GetAllHalls();
+        Hall GetHallById(int id);
+        void AddHall(Hall hall);
+
+        IReadOnlyList<Projection> GetAllProjections();
+        Projection GetProjectionById(int id);
+        void AddProjection(Projection projection);
+
+        IReadOnlyList<Ticket> GetAllTickets();
+        Ticket GetTicketById(int id);
+        void AddTicket(Ticket ticket);
+    }
+}
